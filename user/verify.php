10ac7a6d83d1c359afaae6fe34e527bb12f1968e
mrhiraru <mrhiraru@gmail.com>
2024-01-23 12:39:35 +0800

    update phpmailer

--- user/verify.php
+++ user/verify.php
@@ -16,6 +16,9 @@ require 'vendor/autoload.php';
 //Create an instance; passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
+// Generate 6 digit code
+$verification_code = generate_code();
+
 try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
@@ -28,9 +31,9 @@ try {
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('crimsonavenue@gmail.com', 'Crimson Aveneu');
+    $mail->setFrom('crimsonavenue@gmail.com', 'Crimson Avenue');
     $mail->addAddress($_SESSION['email'], $_SESSION['name']);     //Add a recipient
-    $mail->addReplyTo('crimsonavenue@gmail.com', 'Crimson Aveneu');
+    $mail->addReplyTo('crimsonavenue@gmail.com', 'Crimson Avenue');
 
     //Attachments
     $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
@@ -38,7 +41,7 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Verification Code';
+    $mail->Subject = 'Crimson Avenue Verification';
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
@@ -48,7 +51,7 @@ try {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
-$verification_code = generate_code();
+
 
 ?>
 
