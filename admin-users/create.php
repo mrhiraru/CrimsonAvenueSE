3ed3eb7e499d29f26db2e75f47ee601b8259bd63
mrhiraru <mrhiraru@gmail.com>
2024-02-09 09:17:25 +0800

    Update create.php

--- admin-users/create.php
+++ admin-users/create.php
@@ -95,7 +95,7 @@ include_once('../includes/preloader.php');
                 <main class="col-md-9 col-lg-10 p-4">
                     <div class="row m-0 p-0 h-100">
                         <div class="container-fluid bg-white shadow rounded m-0 p-3 h-100">
-                            <div class="row h-auto d-flex justify-content-start m-0 p-0">
+                            <div class="row h-auto d-flex justify-content-center m-0 pt-2">
                                 <form action="" method="post" class="row d-flex p-2 p-md-0 m-0 col-lg-5">
                                     <div class="mb-3 p-0 col-12">
                                         <input type="email" name="email" placeholder="Email" class="form-control" value="<?php if (isset($_POST['email'])) {
@@ -318,7 +318,7 @@ include_once('../includes/preloader.php');
                                         }
                                         ?>
                                     </div>
-                                    <div class="mb-3 p-0 col-12">
+                                    <div class="p-0 col-12">
                                         <input type="submit" class="btn btn-primary w-100 fw-semibold" name="signup" value="Create Account">
                                     </div>
                                 </form>
