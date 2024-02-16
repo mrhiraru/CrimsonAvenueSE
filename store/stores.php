90d6809b97eef01358823638b1af061967d74c01
mrhiraru <mrhiraru@gmail.com>
2024-02-16 22:31:19 +0800

    stores

--- store/stores.php
+++ store/stores.php
@@ -7,10 +7,8 @@ require_once('../classes/account.class.php');
 require_once('../classes/store.class.php');
 
 $store = new Store();
+
 $limit = 10;
-$page = isset($_GET['page']) ? $_GET['page'] : 1;
-$start = ($page - 1) * $limit;
-$storeArray = $store->show_stores($start, $limit);
 
 $page_count = $store->count_stores();
 $pages = ceil($page_count[0]['store_id'] / $limit);
@@ -24,6 +22,9 @@ if (isset($_SESSION['verification_status']) && $_SESSION['verification_status'] 
 }
 
 
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$start = ($page - 1) * $limit;
+$storeArray = $store->show_stores($start, $limit);
 
 ?>
 
@@ -45,23 +46,27 @@ include_once('../includes/preloader.php');
     <div class="container-fluid col-md-9 mt-4 mx-sm-auto">
         <main>
             <div class="container-fluid bg-white shadow rounded m-0 p-3 h-100">
-                <div class="row m-0 p-0 row-cols-1">
+                <p class="m-0 p-0 fs-2 fw-bold text-primary">
+                    Stores
+                </p>
+                <hr>
+                <div class="row m-0 p-0 grid">
                     <?php
                     $counter = 1;
                     foreach ($storeArray as $item) {
                     ?>
-                        <div class="col-auto p-1 w-100">
-                            <a class="card row col-12 m-0 p-3 store-card bg-white rounded text-decoration-none overflow-hidden" href="./stores.php">
-                                <div class="col-12 col-md-auto m-0 p-0 store-img-cont d-flex justify-content-center align-items-center justify-content-lg-start">
-                                    <img src="../images/main/no-profile.jpg" alt="" class="border border-black border-opacity-10 rounded store-img">
+                        <div class="col-12 col-lg-6 m-0 p-1 d-flex justify-content-center align-items-center">
+                            <a class="card store-card p-3 text-decoration-none overflow-hidden" href="./stores.php">
+                                <div class="row m-0 mb-2 p-0 d-flex align-items-center">
+                                    <div class="col-auto m-0 mb-1 p-0">
+                                        <img src="../images/main/no-profile.jpg" width="60" height="60" alt="" class="border border-secondary border-opacity-25 rounded ">
+                                    </div>
+                                    <div class="col-6 m-0 p-0 ps-3 flex-fill">
+                                        <p class="fs-5 text-nowrap fw-semibold text-primary m-0 text-truncate"><?= ucwords(strtolower($item['store_name'])) ?></p>
+                                    </div>
                                 </div>
-                                <div class="col-12 col-md-6 m-0 p-0 ps-3">
-                                    <div class="col-12 p-0">
-                                        <p class="fs-4 text-nowrap fw-bold text-primary m-0 mb-2 text-truncate"><?= ucwords(strtolower($item['store_name'])) ?></p>
-                                    </div>
-                                    <div class="col-12 p-0 overflow-hidden ">
-                                        <p class="fs-6 text-dark lh-sm m-0 store-bio overflow-hidden "><?= ucfirst(strtolower($item['store_bio'])) ?></p>
-                                    </div>
+                                <div class="row m-0 p-0 overflow-hidden">
+                                    <p class="fs-6 text-dark lh-sm m-0 p-0 store-bio overflow-hidden"><?= (isset($item['store_bio'])) ? ucfirst(strtolower($item['store_bio'])) : 'No bio' ?></p>
                                 </div>
                             </a>
                         </div>
@@ -70,27 +75,29 @@ include_once('../includes/preloader.php');
                     }
                     ?>
                 </div>
-                <nav aria-label="Page navigation example">
-                <ul class="pagination">
-                    <li class="page-item <?= (isset($_GET['page']) && $_GET['page'] <= 1) ? 'disabled' : '' ?>">
-                        <a class="page-link" href="?page=<?= $_GET['page'] - 1 ?>" aria-label="Previous">
-                            <span aria-hidden="true">&laquo;</span>
-                        </a>
-                    </li>
-                    <?php
-                    for ($i = 1; $i <= $pages; $i++) {
-                    ?>
-                        <li class="page-item <?= (isset($_GET['page']) && $_GET['page'] == $i) ? 'active' : ''  ?>"><a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a></li>
-                    <?php
-                    }
-                    ?>
-                    <li class="page-item <?= (isset($_GET['page']) && $_GET['page'] >= $pages) ? 'disabled' : '' ?>">
-                        <a class="page-link" href="?page=<?= $_GET['page'] + 1 ?>" aria-label="Next">
-                            <span aria-hidden="true">&raquo;</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+                <div class="m-2 p-0 d-flex justify-content-center align-items-center">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li class="page-item <?= (isset($_GET['page']) && $_GET['page'] <= 1) ? 'disabled' : '' ?>">
+                                <a class="page-link" href="?page=<?= $_GET['page'] - 1 ?>" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                            </li>
+                            <?php
+                            for ($i = 1; $i <= $pages; $i++) {
+                            ?>
+                                <li class="page-item <?= (isset($_GET['page']) && $_GET['page'] == $i) ? 'active' : ''  ?>"><a class="page-link" href="?page=<?= $i ?>"><?= $i ?></a></li>
+                            <?php
+                            }
+                            ?>
+                            <li class="page-item <?= (isset($_GET['page']) && $_GET['page'] >= $pages) ? 'disabled' : '' ?>">
+                                <a class="page-link" href="?page=<?= $_GET['page'] + 1 ?>" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </main>
         <!-- Extra Section Add more Section if needed ./. -->
