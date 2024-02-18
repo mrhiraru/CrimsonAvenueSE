4c64c523af1c7d2b6d8a9dcde85c08a489c5332d
mrhiraru <mrhiraru@gmail.com>
2024-02-18 19:02:35 +0800

    wew

--- includes/header.store.php
+++ includes/header.store.php
@@ -5,7 +5,7 @@
                 <button class="navbar-toggler d-md-none ms-3 p-0 fs-3 text-light border-0" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                     <i class="fa-solid fa-bars"></i>
                 </button>
-                <a class="navbar-brand h-1 fs-3 fw-bolder ms-3 me-auto d-flex align-items-center text-light" href="../store/index.php">
+                <a class="navbar-brand h-1 fs-3 fw-bolder ms-3 me-auto d-flex align-items-center text-light" href="../store/index.php?store_id=<?= $record['store_id'] ?>">
                     <img src="../images/main/ca-nospace.png" alt="" width="40" height="40" class="d-inline-block me-2">
                     <span class="d-lg-inline d-md-inline d-none"><?= $record['store_name'] ?></span>
                 </a>
@@ -33,7 +33,7 @@
                         </div>
                     </button>
                     <ul class="dropdown-menu dropdown-menu-end me-2 mt-2">
-                        <li><a class="dropdown-item text-secondary fw-bold py-1 px-3 " href="../user/profile.php">Exit Store</a></li>
+                        <li><a class="dropdown-item text-secondary fw-bold py-1 px-3 " href="../stores/my-stores.php">Exit Store</a></li>
                         <li><a class="dropdown-item text-secondary fw-bold py-1 px-3 " href="../logout.php">Log Out</a></li>
                     </ul>
                 </div>
