f41ae8624b95eb1a88b93e5e343d6e2307d2613e
mrhiraru <mrhiraru@gmail.com>
2024-02-20 18:26:59 +0800

    232

--- classes/product.class.php
+++ classes/product.class.php
@@ -23,9 +23,12 @@ class Product
 
     function add()
     {
+        $connect = $this->db->connect();
+        $connect->beginTransaction();
+
         $sql = "INSERT INTO product (store_id, category_id, product_name, exclusivity, sale_status) VALUES (:store_id, :category_id, :product_name, :exclusivity, :sale_status,)";
 
-        $query = $this->db->connect()->prepare($sql);
+        $query = $connect->prepare($sql);
         $query->bindParam(':store_id', $this->store_id);
         $query->bindParam(':product_name', $this->product_name);
         $query->bindParam(':category_id', $this->category_id);
@@ -33,8 +36,29 @@ class Product
         $query->bindParam(':sale_status', $this->sale_status);
 
         if ($query->execute()) {
-            return true;
+            $last_product_id = $connect->lastInsertId();
+
+            $sec_sql = "INSERT INTO variation (product_id, variation_name) VALUES (:product_id, :variation_name)";
+
+            $sec_query = $connect->prepare($sec_sql);
+            $sec_query->bindParam(':product_id', $last_product_id);
+            $sec_query->bindParam(':variation_name', 'Default');
+
+            $thi_sql = "INSERT INTO measurement (product_id, measurement_name) VALUES (:product_id, :measurement_name)";
+
+            $thi_query = $connect->prepare($thi_sql);
+            $thi_query->bindParam(':product_id', $last_product_id);
+            $thi_query->bindParam(':measurement_name', 'Default');
+
+            if ($sec_query->execute() && $thi_query->execute()) {
+                $connect->commit();
+                return true;
+            } else {
+                $connect->rollBack();
+                return false;
+            }
         } else {
+            $connect->rollBack();
             return false;
         }
     }
