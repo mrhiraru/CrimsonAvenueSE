f992ede4b5ccd8623a098ffaef25cfd2ad6d2915
mrhiraru <mrhiraru@gmail.com>
2024-02-24 11:00:59 +0800

    23232

--- store-product/product-view.php
+++ store-product/product-view.php
@@ -177,8 +177,93 @@ include_once('../includes/preloader.php');
                     </div>
                     <div class="container-fluid bg-white shadow rounded m-0 mt-4 p-3">
                         <div class="row d-flex justify-content-between m-0 p-0">
+                            <div class="col-12 m-0 p-0 px-2">
+                                <p class="m-0 p-0 fs-4 fw-bold text-primary lh-1 flex-fill">
+                                    Descriptions
+                                </p>
+                            </div>
+                            <div class="col-12 m-0 p-0">
+                                <hr class="mb-3">
+                            </div>
+                            <form method="post" action="" class="col-12 col-lg-4">
+                                <div class="row">
+                                    <div class="input-group mb-2 p-0 col-12">
+                                        <?php
+                                        if (isset($_POST['edit']) || isset($_POST['save'])) {
+                                            $record = $college->fetch($_GET['id']);
+                                        ?>
+                                            <input type="text" class="form-control" id="col-name" name="col-name" placeholder="College Name" value="<?php if (isset($_POST['col-name'])) {
+                                                                                                                                                        echo $_POST['col-name'];
+                                                                                                                                                    } else {
+                                                                                                                                                        echo $record['college_name'];
+                                                                                                                                                    } ?>">
+                                            <input type="submit" class="btn btn-primary-opposite btn-settings-size fw-semibold" id="basic-addon1" name="cancel" value="Cancel">
+                                            <input type="submit" class="btn btn-primary btn-settings-size fw-semibold" id="basic-addon2" name="save" value="Save">
+                                        <?php
+                                        } else {
+                                        ?>
+                                            <input type="text" class="form-control" id="col-name" name="col-name" placeholder="College Name" value="<?php if (isset($_POST['col-name'])) {
+                                                                                                                                                        echo $_POST['col-name'];
+                                                                                                                                                    } ?>">
+                                            <input type="submit" class="btn btn-primary btn-settings-size fw-semibold" id="basic-addon1" name="add" value="Add">
+                                        <?php
+                                        }
+                                        ?>
+                                    </div>
+                                    <?php
+                                    if (isset($_POST['add']) && isset($_POST['col-name']) && !validate_field($_POST['col-name'])) {
+                                    ?>
+                                        <div class="mb-2 col-auto mb-2 p-0">
+                                            <p class="fs-7 text-primary mb-2 ps-2">College name is required.</p>
+                                        </div>
+                                    <?php
+                                    } else if (isset($_POST['save']) && isset($_POST['col-name']) && !validate_field($_POST['col-name'])) {
+                                    ?>
+                                        <div class="mb-2 col-auto mb-2 p-0">
+                                            <p class="fs-7 text-primary mb-2 ps-2">Update failed! College name is required.</p>
+                                        </div>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
+                            </form>
                             <div class="col-12 m-0 p-0 px-2 row">
-                                Descriptions Here
+                                <table id="products" class="table table-lg mt-1">
+                                    <thead>
+                                        <tr class="align-middle">
+                                            <th scope="col"></th>
+                                            <th scope="col">Label</th>
+                                            <th scope="col">Value</th>
+                                            <th scope="col" class="text-center">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                        $counter = 1;
+                                        $product = new Product();
+                                        $productArray = $product->show($record['store_id']);
+                                        foreach ($productArray as $item) {
+                                        ?>
+                                            <tr class="align-middle">
+                                                <td><?= $counter ?></td>
+                                                <td><?= $item['category_name'] ?></td>
+                                                <td><?= $item['exclusivity'] ?></td>
+                                                <td class="text-center text-nowrap">
+                                                    <div class="m-0 p-0">
+                                                        <form action="<?php // edit action link 
+                                                                        ?>" method="post">
+                                                            <input type="submit" class="btn btn-primary btn-settings-size py-1 px-2 rounded border-0 fw-semibold" id="college-edit" name="edit" value="Edit"></input>
+                                                            <input type="submit" class="btn btn-primary-opposite btn-settings-size py-1 px-2 rounded border-0 fw-semibold" name="warning" value="Delete"></input>
+                                                        </form>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        <?php
+                                            $counter++;
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
