43f1ea6fea13dfafc82bd4e9834b131c4355d898
mrhiraru <mrhiraru@gmail.com>
2024-03-13 04:40:29 +0800

    2

--- store-product/create.php
+++ store-product/create.php
@@ -21,7 +21,11 @@ if (isset($_POST['create'])) {
     $product->product_name = htmlentities($_POST['product_name']);
     $product->category_id = htmlentities($_POST['category_id']);
     $product->exclusivity = htmlentities($_POST['exclusivity']);
-    $product->sale_status = htmlentities($_POST['sale_status']);
+    if (isset($_POST['sale_status'])) {
+        $product->sale_status = htmlentities($_POST['sale_status']);
+    } else {
+        $product->sale_status = '';
+    }
     $product->store_id = $_GET['store_id'];
 
     if (
