8725647fe2cf7a458dc05b571dfeca9336704b0f
mrhiraru <mrhiraru@gmail.com>
2024-03-18 21:14:40 +0800

    23

---
+++ products/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+// Change title for each page.
+$title = "Products  | Crimson Avenue";
+$product_page = "active";
+require_once('../includes/head.php');
+include_once('../includes/preloader.php');
+?>
+
+<body>
+    <?php
+    require_once('../includes/header.user.php');
+    ?>
+    <div class="container-fluid col-md-9 mt-4 mx-sm-auto">
+        <main>
+            <div class="container-fluid bg-white shadow rounded m-0 p-3 h-100">
+                <div class="row m-0 p-0 d-flex align-items-center">
+                    <div class="col-6 m-0 p-0">
+                        <p class="m-0 p-0 fs-2 fw-bold text-primary lh-1">Products</p>
+                    </div>
+                </div>
+                <hr>
+                <div class="row m-0 p-0 grid">
+                    <!-- product cards here -->
+                </div>
+                <div class="m-2 p-0 d-flex justify-content-center align-items-center">
+                    <!-- pagination here // copy from stores/stores.php -->
+                </div>
+            </div>
+        </main>
+        <section>
+            <!-- Code Here Extra Section -->
+        </section>
+        <!-- Extra Section Add more Section if needed ./. -->
+    </div>
+    <?php
+    require_once('../includes/js.php');
+    ?>
+</body>
+
+</html>
