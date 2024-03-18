d12f0660f6d663c7d0d34817c428a457efa354f9
mrhiraru <mrhiraru@gmail.com>
2024-03-19 01:14:09 +0800

    2

--- includes/sidepanel.product.php
+++ includes/sidepanel.product.php
@@ -1,4 +1,4 @@
-<nav id="sidebarMenu" class="col-md-auto d-md-block collapse">
+<nav id="sidebarMenu" class="col-md-auto pt-5 d-md-block collapse">
     <div class="position-sticky pt-3 min-vh-custom">
         <div class="accordion accordion-flush" id="accordionFlushExample">
             <div class="accordion-item border-0">
@@ -34,7 +34,7 @@
                     <div class="accordion-body px-0 ps-2 py-1 fs-7 text-secondary border-0">
                         <form action="">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
                                 <label class="form-check-label" for="flexRadioDefault1">
                                     Relevance
                                 </label>
@@ -79,7 +79,7 @@
                     <div class="accordion-body px-0 ps-2 py-1 fs-7 text-secondary border-0">
                         <form action="">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
                                 <label class="form-check-label" for="flexRadioDefault1">
                                     All Users
                                 </label>
@@ -93,7 +93,7 @@
                             <div class="form-check">
                                 <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
                                 <label class="form-check-label" for="flexRadioDefault1">
-                                    Department 
+                                    Department
                                     <!-- change to department name  -->
                                 </label>
                             </div>
@@ -102,6 +102,52 @@
                                 <label class="form-check-label" for="flexRadioDefault1">
                                     College
                                     <!-- change to college name -->
+                                </label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item border-0">
+                <h2 class="accordion-header bg-smoke" id="flush-headingOne">
+                    <button class="fs-6 bg-smoke border-0 px-2 py-1" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="true" aria-controls="flush-collapseOne">
+                        <p class="nav-link text-secondary fw-semibold m-0" aria-current="page">
+                            Rating
+                        </p>
+                    </button>
+                </h2>
+                <div id="flush-collapseOne" class="accordion-collapse collapse show bg-smoke" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+                    <div class="accordion-body px-0 ps-2 py-1 fs-7 text-secondary border-0">
+                        <form action="">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    ★★★★★
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    ★★★★☆ & Up
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    ★★★☆☆ & Up
+                                </label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    ★★☆☆☆ & Up
+                                </label>
+                            </div>
+
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                <label class="form-check-label" for="flexRadioDefault1">
+                                    ★☆☆☆☆ & Up
                                 </label>
                             </div>
                         </form>
