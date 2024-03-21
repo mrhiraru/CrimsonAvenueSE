f894a0151e21772b6c24177d44252ecf29c795da
mrhiraru <mrhiraru@gmail.com>
2024-03-21 09:27:18 +0800

    2

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
-<div class="" style="height: 200px; width: 100%; background-color: red;">
-    This is Footer
+<div class="text-center fs-1 text-white fw-bold" style="height: 200px; width: 100%; background-color: red;">
+   ♣ ♠ ♦ ♥ This is Footer ♥ ♦ ♠ ♣   
 </div>
