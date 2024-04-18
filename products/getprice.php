15baa22e599737dd88173afa6890bdbdfae2c1dd
mrhiraru <mrhiraru@gmail.com>
2024-04-18 22:52:34 +0800

    2

--- products/getprice.php
+++ products/getprice.php
@@ -17,7 +17,7 @@ $price_record = $stock->price_fetch($variation_id, $measurement_id, $product_id)
 
 if ($record['sale_status'] == "On-hand" && isset($stock_record['selling_price'])) {
 
-    echo "₱ " . number_format($stock['selling_price'], 2, '.', ',');
+    echo "₱ " . number_format($stock_record['selling_price'], 2, '.', ',');
 } else if ($record['sale_status'] == "Pre-order" && isset($price_record['selling_price'])) {
 
     echo "₱ " . number_format($price_record['selling_price'], 2, '.', ',');
