ae9140b372027319d0de884c4e05ea9ce35d71e5
WilfredBorjal <memerpatrick1@gmail.com>
2024-04-22 22:42:34 +0800

    cmd
    cmt

--- stores/store-view.php
+++ stores/store-view.php
@@ -1,6 +1,7 @@
 <?php 
 require_once('../tools/functions.php');
 require_once('../classes/store.class.php');
+session_start();
 
 $store = new Store();
 $record = $store->fetch($_GET['store_id']);
@@ -23,9 +24,26 @@ include_once('../includes/preloader.php');
     ?>
     <div class="container-fluid col-md-9 mt-4 mx-sm-auto min-vh-100 ">
         <main>
-
+            <div class="container shadow">
+                <div class="row ">
+                    <div class="col-lg-6 p-5 rounded ">
+                        <div class="row bg-maroon py-3 ps-2 ">
+                            <div class="col-lg-6  ">
+                                <img src="../images/main/profilepic.png" class= " rounded-circle ms-5" width="200px" alt="Cinque Terre">
+                            </div>
+                            <div class="col-lg-6">
+                                <p class="h3 fw-bolder ">CCS IHAWAN</p>
+                                <p class="h5">Department</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <p class="h1"></p>
+                    </div>
+                </div>
+            </div>
         </main>
-
+        
         
 
         <section>
